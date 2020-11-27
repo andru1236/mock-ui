0593ff45eea08c48208f1ea4386fd5821e6b8541
Gutierrez, Andres <andres.gutierrez@jalasoft.com>
2020-11-27 02:10:13 -0400

    refactor table of paths

--- src/packages/routes/components/table/PathsTable.tsx
+++ src/packages/routes/components/table/PathsTable.tsx
@@ -2,45 +2,43 @@ import React from 'react'
 import { Table } from 'semantic-ui-react'
 
 import MethodButton from "./MethodButton";
-import { IApiInstance } from "../../../../domain/api";
-import { withPathConsumer } from "../PathContext";
 
-interface IViewProps {
-    selectedApi: IApiInstance;
-    reloadSelectedApi (): void;
-}
+import { withPathConsumer, PathContextProps } from "../PathContext";
 
-const PathsTable = (props: IViewProps) => (
-    <Table compact={ 'very' } basic={ 'very' } celled collaping={ 'true' }>
-        <Table.Header>
-            <Table.Row>
-                <Table.HeaderCell>Path</Table.HeaderCell>
-                <Table.HeaderCell>methods</Table.HeaderCell>
-            </Table.Row>
-        </Table.Header>
 
-        <Table.Body>
-            { props.selectedApi.routes.map((route) => {
-                return (
-                    <Table.Row verticalAlign='top' key={ route.path }>
-                        <Table.Cell> { route.path }</Table.Cell>
-                        <Table.Cell>
-                            { route.resources.map((resource) => {
-                                return (
-                                    <MethodButton
-                                        resource={ resource }
-                                        key={ resource.method }
-                                        path={ route }
-                                        reloadApis={ props.reloadSelectedApi }
-                                    />
-                                );
-                            }) }
-                        </Table.Cell>
-                    </Table.Row>
-                );
-            }) }
-        </Table.Body>
-    </Table>
+const PathsTable = ({ selectedApi, reloadSelectedApi }: PathContextProps) => (
+  <Table compact={ 'very' } basic={ 'very' } celled collaping={ 'true' }>
+
+      <Table.Header>
+          <Table.Row>
+              <Table.HeaderCell>Path</Table.HeaderCell>
+              <Table.HeaderCell>methods</Table.HeaderCell>
+          </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+          { selectedApi.routes.map((route) => {
+              return (
+                <Table.Row verticalAlign='top' key={ route.path }>
+                    <Table.Cell> { route.path }</Table.Cell>
+                    <Table.Cell>
+                        { route.resources.map((resource) => {
+                            return (
+                              <MethodButton
+                                resource={ resource }
+                                key={ resource.method }
+                                path={ route }
+                                reloadApis={ reloadSelectedApi }
+                              />
+                            );
+                        }) }
+                    </Table.Cell>
+                </Table.Row>
+              );
+          }) }
+      </Table.Body>
+
+  </Table>
 );
 
 export default withPathConsumer(PathsTable);
